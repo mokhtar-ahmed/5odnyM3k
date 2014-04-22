0bad82b37e32f939f06e08910a7f177bf8f46f91
SarahMohamed268 <sara.mohamed269@gmail.com>
2014-04-22 11:09:40 -0700

    SyncContactsService Frontend part

--- src/com/iti/jets/carpoolingV1/httphandler/HttpConstants.java
+++ src/com/iti/jets/carpoolingV1/httphandler/HttpConstants.java
@@ -1,5 +1,34 @@
 package com.iti.jets.carpoolingV1.httphandler;
 
+import android.util.Log;
+
 public class HttpConstants {
 
+	private static String urii = new String();
+	
+	public HttpConstants()
+	{
+		
+	}
+	
+	public String getWebserviceURI()
+	{
+		
+		
+		return this.urii;
+		
+	}
+	
+	public  void setWebserviceURI(String uri)
+	{
+		
+		
+		this.urii = uri;
+		
+		
+	}
+	
+	
+	
+	
 }
--- src/com/iti/jets/carpoolingV1/jsonhandler/JsonConverter.java
+++ src/com/iti/jets/carpoolingV1/jsonhandler/JsonConverter.java
@@ -1,5 +1,41 @@
 package com.iti.jets.carpoolingV1.jsonhandler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.iti.jets.carpoolingV1.common.User;
+
 public class JsonConverter {
+	
+	
+	
+	public JSONObject objectToJSON (User user)
+	{
+		JSONObject tempJasonObj = new JSONObject();
+		try {			
+			tempJasonObj.put("name", user.getName());
+			tempJasonObj.put("phone", user.getPhone());
+			tempJasonObj.put("userId", user.getUserId());
+			return tempJasonObj;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();			
+		}
+		return tempJasonObj;
+	}
+	
+	public JSONArray arrayListToJSONArray (ArrayList<String> arrayList)
+	{
+		JSONArray jsonArray = new JSONArray(arrayList);
+		return jsonArray;
+	}
+	
+
 
 }
