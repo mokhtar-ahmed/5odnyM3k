642843c2016eba9ef4107d4448a1ab3d7aa78673
SarahMohamed268 <sara.mohamed269@gmail.com>
2014-04-23 00:09:51 -0700

    EditProfileService Frontend Part

--- gen/com/iti/jets/carpoolingV1/R.java
+++ gen/com/iti/jets/carpoolingV1/R.java
@@ -21,29 +21,56 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg1=0x7f020000;
+        public static final int bgn7=0x7f020001;
+        public static final int calendaricon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int photo=0x7f020004;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080001;
-        public static final int ScrollView1=0x7f080000;
-        public static final int action_settings=0x7f080004;
-        public static final int contactTextView1=0x7f080002;
-        public static final int menu_settings=0x7f080003;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int LinearLayout2=0x7f08000b;
+        public static final int NametextView=0x7f080008;
+        public static final int ScrollView1=0x7f08000e;
+        public static final int action_settings=0x7f080011;
+        public static final int contactTextView1=0x7f08000f;
+        public static final int dateEditText=0x7f080002;
+        public static final int dateTextView=0x7f08000c;
+        public static final int doneBtn=0x7f080003;
+        public static final int editdateImgView=0x7f08000d;
+        public static final int editnameImgView=0x7f080009;
+        public static final int menu_settings=0x7f080010;
+        public static final int saveBtn=0x7f080005;
+        public static final int textView1=0x7f080007;
+        public static final int textView3=0x7f08000a;
+        public static final int textView5=0x7f080001;
+        public static final int userImgView=0x7f080006;
+        public static final int usernameEditText=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_sync_contacts=0x7f030001;
-        public static final int activity_sync_contacts2=0x7f030002;
+        public static final int activity_edit_date=0x7f030000;
+        public static final int activity_edit_name=0x7f030001;
+        public static final int activity_edit_profile=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_sync_contacts=0x7f030004;
+        public static final int activity_sync_contacts2=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_sync_contacts=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int activity_edit_date=0x7f070000;
+        public static final int activity_edit_name=0x7f070001;
+        public static final int activity_edit_profile=0x7f070002;
+        public static final int activity_sync_contacts=0x7f070003;
+        public static final int main=0x7f070004;
     }
     public static final class string {
+        public static final int DatePicker=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int menu_settings=0x7f050003;
+        public static final int title_activity_edit_date=0x7f050007;
+        public static final int title_activity_edit_name=0x7f050006;
+        public static final int title_activity_edit_profile=0x7f050005;
         public static final int title_activity_sync_contacts=0x7f050004;
     }
     public static final class style {
